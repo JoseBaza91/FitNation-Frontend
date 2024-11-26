5f246df91c28b66403e62746d70d84316af51ccd
Juan Pablo de la Vega <juanpablo.miscritop@gmail.com>
2024-11-26 19:50:46 -0300

    se hacen cambios en PaginaAdminInicio, usando el component TableC

--- src/pages/PaginaAdminInicio.jsx
+++ src/pages/PaginaAdminInicio.jsx
@@ -1,19 +1,19 @@
-import { Col, Container, Row } from "react-bootstrap";
 import TableC from "../components/TableC";
+import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 const PaginaAdminInicio = () => {
   return (
     <>
-    <Container><h1 id="mensaje-admin">Hola, Administrador</h1></Container>
+    <Container className="mt-3"><h1 id="mensaje-admin">Hola, Administrador</h1></Container>
     <TableC/>
-    <Container class="d-flex justify-content-between pe-5 ps-5">
-      <Link to="/admin-clases">
-    <Button variant="primary" size="lg">Ir a Clases</Button>{' '}
+    <Container className="d-flex justify-content-evenly pt-5">
+    <Link to="/admin-clases">
+    <Button variant="primary">Ir a Clases</Button>
     </Link>
     <Link to="/admin-usuarios">
-    <Button variant="primary" size="lg">Ir a Usuarios</Button>{' '}
+    <Button variant="primary">Ir a Usuarios</Button>
     </Link>
     </Container>
     </>
